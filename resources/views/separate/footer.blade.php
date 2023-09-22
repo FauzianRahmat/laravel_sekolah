cacf47c875e53700a94852ddbc8a76a0f60882ba
fauzianrahmat <zyanrahmat24@gmail.com>
2023-09-22 07:42:24 +0700

    mengupdate crud

--- resources/views/separate/footer.blade.php
+++ resources/views/separate/footer.blade.php
@@ -4,6 +4,6 @@
         Anything you want
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2023 <a href="https://www.instagram.com/dmrrstyo">Dmar</a>.</strong> All rights
+    <strong>Copyright &copy; 2023 <a href="https://www.instagram.com/dmrrstyo">zyan</a>.</strong> All rights
     reserved.
 </footer>
